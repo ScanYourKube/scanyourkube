244e476540b8a38e1cdb61326ebd5a016f39b005
Alex Trittibach <alex.trittibach@ti8m.ch>
2023-06-06 17:29:17 +0200

    Add test case for my http client

---
+++ cronjob/api/http_client/my_http_client_test.go
@@ -0,0 +1,151 @@
+package http_client_test
+
+import (
+	"bytes"
+	"io/ioutil"
+	"net/http"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"http_client"
+)
+
+func TestMyHttpClientGet(t *testing.T) {
+	// Create a test HTTP client with a mock Do function
+	mockClient := &http.Client{
+		Transport: &mockTransport{
+			roundTripFunc: func(req *http.Request) (*http.Response, error) {
+				// Verify that the request is a GET request
+				assert.Equal(t, http.MethodGet, req.Method)
+
+				// Create a mock response
+				response := &http.Response{
+					StatusCode: http.StatusOK,
+					Body:       ioutil.NopCloser(bytes.NewBufferString("OK")),
+				}
+
+				return response, nil
+			},
+		},
+	}
+
+	// Create an instance of MyHttpClient using the mock client
+	client := http_client.NewMyHttpClient(mockClient)
+
+	// Perform the GET request
+	response, err := client.Get("http://example.com")
+
+	// Verify the response and error
+	assert.NoError(t, err)
+	assert.Equal(t, http.StatusOK, response.StatusCode)
+	body, _ := ioutil.ReadAll(response.Body)
+	assert.Equal(t, "OK", string(body))
+}
+
+func TestMyHttpClientPost(t *testing.T) {
+	// Create a test HTTP client with a mock Do function
+	mockClient := &http.Client{
+		Transport: &mockTransport{
+			roundTripFunc: func(req *http.Request) (*http.Response, error) {
+				// Verify that the request is a POST request
+				assert.Equal(t, http.MethodPost, req.Method)
+
+				// Verify the request body
+				body, _ := ioutil.ReadAll(req.Body)
+				assert.Equal(t, []byte(`{"key":"value"}`), body)
+
+				// Create a mock response
+				response := &http.Response{
+					StatusCode: http.StatusOK,
+					Body:       ioutil.NopCloser(bytes.NewBufferString("OK")),
+				}
+
+				return response, nil
+			},
+		},
+	}
+
+	// Create an instance of MyHttpClient using the mock client
+	client := http_client.NewMyHttpClient(mockClient)
+
+	// Perform the POST request
+	err := client.Post("http://example.com", []byte(`{"key":"value"}`))
+
+	// Verify the error
+	assert.NoError(t, err)
+}
+
+func TestMyHttpClientPut(t *testing.T) {
+	// Create a test HTTP client with a mock Do function
+	mockClient := &http.Client{
+		Transport: &mockTransport{
+			roundTripFunc: func(req *http.Request) (*http.Response, error) {
+				// Verify that the request is a PUT request
+				assert.Equal(t, http.MethodPut, req.Method)
+
+				// Verify the request body
+				body, _ := ioutil.ReadAll(req.Body)
+				assert.Equal(t, []byte(`{"key":"value"}`), body)
+
+				// Create a mock response
+				response := &http.Response{
+					StatusCode: http.StatusOK,
+					Body:       ioutil.NopCloser(bytes.NewBufferString("OK")),
+				}
+
+				return response, nil
+			},
+		},
+	}
+
+	// Create an instance of MyHttpClient using the mock client
+	client := http_client.NewMyHttpClient(mockClient)
+
+	// Perform the PUT request
+	response, err := client.Put("http://example.com", []byte(`{"key":"value"}`))
+
+	// Verify the response and error
+	assert.NoError(t, err)
+	assert.Equal(t, http.StatusOK, response.StatusCode)
+	body, _ := ioutil.ReadAll(response.Body)
+	assert.Equal(t, "OK", string(body))
+}
+
+func TestMyHttpClientDelete(t *testing.T) {
+	// Create a test HTTP client with a mock Do function
+	mockClient := &http.Client{
+		Transport: &mockTransport{
+			roundTripFunc: func(req *http.Request) (*http.Response, error) {
+				// Verify that the request is a DELETE request
+				assert.Equal(t, http.MethodDelete, req.Method)
+
+				// Create a mock response
+				response := &http.Response{
+					StatusCode: http.StatusOK,
+					Body:       ioutil.NopCloser(bytes.NewBufferString("OK")),
+				}
+
+				return response, nil
+			},
+		},
+	}
+
+	// Create an instance of MyHttpClient using the mock client
+	client := http_client.NewMyHttpClient(mockClient)
+
+	// Perform the DELETE request
+	err := client.Delete("http://example.com")
+
+	// Verify the error
+	assert.NoError(t, err)
+}
+
+// mockTransport is a custom RoundTripper that allows mocking HTTP requests and responses
+type mockTransport struct {
+	roundTripFunc func(req *http.Request) (*http.Response, error)
+}
+
+func (t *mockTransport) RoundTrip(req *http.Request) (*http.Response, error) {
+	return t.roundTripFunc(req)
+}
